c6b9036f68708ffe23e57d7c929676a0f1135921
Chrivent <yechandubai@gmail.com>
2025-03-22 23:48:26 +0900

    [#42] 위젯 생성 임시 방편

--- Source/Project_RCN/Private/Game/RCN_MultiModeBase.cpp
+++ Source/Project_RCN/Private/Game/RCN_MultiModeBase.cpp
@@ -14,14 +14,9 @@ void ARCN_MultiModeBase::PostLogin(APlayerController* NewPlayer)
 {
 	Super::PostLogin(NewPlayer);
 	
-	FTimerHandle TimerHandle;
-	GetWorldTimerManager().SetTimer(TimerHandle, FTimerDelegate::CreateWeakLambda(this, [=, this]
+	FTimerHandle TimerHandle1;
+	GetWorldTimerManager().SetTimer(TimerHandle1, FTimerDelegate::CreateWeakLambda(this, [=, this]
 	{
-		if (ARCN_PlayerController* NewPlayerController = Cast<ARCN_PlayerController>(NewPlayer))
-		{
-			NewPlayerController->CreateTimerWidget();
-		}
-		
 		if (ARCN_RubikCube* RubikCube = Cast<ARCN_RubikCube>(GetWorld()->SpawnActor(GameModeBaseDataAsset->RubikCubeClass)))
 		{
 			RubikCube->SetOwner(NewPlayer->GetPawn());
@@ -33,34 +28,45 @@ void ARCN_MultiModeBase::PostLogin(APlayerController* NewPlayer)
 				Player->UpdateCubeLocation(FVector::ForwardVector * GameModeBaseDataAsset->CubeStartDistance);
 				Player->UpdateCubeRotation(GameModeBaseDataAsset->CubeStartRotation);
 			}
-			
-			for (auto Iterator = GetWorld()->GetPlayerControllerIterator(); Iterator; ++Iterator)
-			{
-				if (ARCN_Player* MultiPlayer = Cast<ARCN_Player>(Iterator->Get()->GetPawn()))
-				{
-					MultiPlayer->RenewalCube();
-				}
-
-				if (Iterator->Get() != NewPlayer)
-				{
-					const ARCN_PlayerController* OtherPlayerController = Cast<ARCN_PlayerController>(Iterator->Get());
-					const ARCN_PlayerController* NewPlayerController = Cast<ARCN_PlayerController>(NewPlayer);
-					if (IsValid(OtherPlayerController) && IsValid(NewPlayerController))
-					{
-						ARCN_Player* OtherPlayer = Cast<ARCN_Player>(OtherPlayerController->GetPawn());
-						ARCN_Player* Player = Cast<ARCN_Player>(NewPlayerController->GetPawn());
-						if (IsValid(OtherPlayer) && IsValid(Player))
-						{
-							OtherPlayer->CreateOtherPlayerViewWidget(Player);
-							Player->CreateOtherPlayerViewWidget(OtherPlayer);
-						}
-					}
-				}
-			}
 
 			// Todo: FinishScrambleDelegate를 어떻게 연결할지 생각할 필요가 있음
 		}
+
+		for (auto Iterator = GetWorld()->GetPlayerControllerIterator(); Iterator; ++Iterator)
+		{
+			if (ARCN_Player* MultiPlayer = Cast<ARCN_Player>(Iterator->Get()->GetPawn()))
+			{
+				MultiPlayer->RenewalCube();
+			}
+		}
+		
+		if (ARCN_PlayerController* NewPlayerController = Cast<ARCN_PlayerController>(NewPlayer))
+		{
+			NewPlayerController->CreateTimerWidget();
+		}
 	}), 1.0f, false);
+
+	FTimerHandle TimerHandle2;
+	GetWorldTimerManager().SetTimer(TimerHandle2, FTimerDelegate::CreateWeakLambda(this, [=, this]
+	{
+		for (auto Iterator = GetWorld()->GetPlayerControllerIterator(); Iterator; ++Iterator)
+		{
+			if (Iterator->Get() != NewPlayer)
+			{
+				const ARCN_PlayerController* NewPlayerController = Cast<ARCN_PlayerController>(NewPlayer);
+				const ARCN_PlayerController* OtherPlayerController = Cast<ARCN_PlayerController>(Iterator->Get());
+				if (IsValid(OtherPlayerController) && IsValid(NewPlayerController))
+				{
+					ARCN_Player* Player = Cast<ARCN_Player>(NewPlayerController->GetPawn());
+					ARCN_Player* OtherPlayer = Cast<ARCN_Player>(OtherPlayerController->GetPawn());
+					if (IsValid(Player) && IsValid(OtherPlayer))
+					{
+						Player->CreateOtherPlayerViewWidget(OtherPlayer);
+					}
+				}
+			}
+		}
+	}), 2.0f, false);
 }
 
 AActor* ARCN_MultiModeBase::ChoosePlayerStart_Implementation(AController* Player)
